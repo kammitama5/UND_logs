69e59c48e5419a1eed68231053a0faad87597bfb
kammitama5 <krystal.maughan@gmail.com>
2017-10-31 22:23:07 -0700

    updated based on reviewer comments

--- logs.py
+++ logs.py
@@ -1,4 +1,4 @@
-# ! /usr/bin/env python
+#!/usr/bin/env python
 
 # import psycopg2
 import psycopg2
@@ -11,81 +11,95 @@ DBNAME = "news"
 # execute query, fetch and close database request
 
 def connect_query(query):
-    db = psycopg2.connect(database=DBNAME)
-    cursor = db.cursor()
-    cursor.execute(query)
-    return cursor.fetchall()
-    db.close()
+    """ connect to postgreSQL database. Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database=DBNAME))
+        cursor = db.cursor()
+        db, c = connect()
+        cursor.execute(query)
+        result =  cursor.fetchall()
+        db.close()
+        return result
+    except: psycopg2.Error as e:
+        print "Unable to connect to database"
+        # Then exit the program
+        sys.exit(1)
+        
+        
+        
+# Then you can use db, c = connect()
+
 # list queries
 # Query 1: What are the three most popular articles of all time?
 
+if __name__ == '__main__':
 
-query_1 = """
-
-
-select title, count(*) as views from articles inner join
-log on concat('/article/', articles.slug) = log.path
-where log.status like '%200%'
-group by log.path, articles.title order by views desc limit 3;
-"""
+    query_1 = """
 
 
-# function for first query
-def query_one(query):
-    # function connects to database and runs first query
-    queries_ = connect_query(query)
-    print "What are the most popular three articles of all time?"
-    print "The 3 most popular articles of all time are: "
-    for query_ in queries_:
-        print "{} - {} views".format(query_[0], query_[1])
-# Query 2
-# Who are the most popular article authors of all time?
-
-
-query_2 = """
-
-
-select authors.name, count(*) as views from articles inner join
-authors on articles.author = authors.id inner join
-log on concat('/article/', articles.slug) = log.path where
-log.status like '%200%' group by authors.name order by views desc
-"""
-
-# query_2 function
-
-
-def query_two(query):
-
+    select title, count(*) as views from articles inner join
+    log on concat('/article/', articles.slug) = log.path
+    where log.status like '%200%'
+    group by log.path, articles.title order by views desc limit 3;
     """
-    function connects to database and runs second query
+
+
+    # function for first query
+    def query_one(query):
+        # function connects to database and runs first query
+        queries_ = connect_query(query)
+        print "What are the most popular three articles of all time?"
+        print "The 3 most popular articles of all time are: "
+        for query_ in queries_:
+            print "{} - {} views".format(query_[0], query_[1])
+    # Query 2
+    # Who are the most popular article authors of all time?
+
+
+    query_2 = """
+
+
+    select authors.name, count(*) as views from articles inner join
+    authors on articles.author = authors.id inner join
+    log on concat('/article/', articles.slug) = log.path where
+    log.status like '%200%' group by authors.name order by views desc
     """
-    queries_ = connect_query(query)
-    print "Who are the most popular article authors of all time?"
-    print "The most popular article authors of all time are : "
-    for query_ in queries_:
-        print "{} - {}views".format(query_[0], query_[1])
-# Query 3
-# On Which days did more than 1% of requests lead to errors?
+
+    # query_2 function
 
 
-query_3 = """
+    def query_two(query):
+
+        """
+        function connects to database and runs second query
+        """
+        queries_ = connect_query(query)
+        print "Who are the most popular article authors of all time?"
+        print "The most popular article authors of all time are : "
+        for query_ in queries_:
+            print "{} - {}views".format(query_[0], query_[1])
+    # Query 3
+    # On Which days did more than 1% of requests lead to errors?
 
 
-select * from (
-select request1.day,
-round(cast((100*request2.req) as numeric) /
-cast(request1.req as numeric), 2)
-    as err from
-    (select date(time) as day,
-    count(*) as req from log group by day) as request1
-        inner join
-        (select date(time) as day, count(*) as req from log where status
-        like '%404%' group by day) as request2
-    on request1.day = request2.day)
-as threshold where err > 1.0;
-"""
+    query_3 = """
 
-# query_3 function
+
+    select * from (
+    select request1.day,
+    round(cast((100*request2.req) as numeric) /
+    cast(request1.req as numeric), 2)
+        as err from
+        (select date(time) as day,
+        count(*) as req from log group by day) as request1
+            inner join
+            (select date(time) as day, count(*) as req from log where status
+            like '%404%' group by day) as request2
+        on request1.day = request2.day)
+    as threshold where err > 1.0;
+    """
+
+    # query_3 function
 
 
 def query_three(query):
